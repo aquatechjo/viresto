6ed055ac063b6dbca2e5bb38e1639ec5a2fa9fec
Aqua Tech <info.aquatech.jo@gmail.com>
2026-07-05 20:59:45 +0300

    Update sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -11,12 +11,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1,
     },
     {
-      url: `${siteUrl}/login`,
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.5,
-    },
-    {
       url: `${siteUrl}/register`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
